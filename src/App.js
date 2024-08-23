3728abe6f2ef568062f827f162eebf9115d11aa5
Bernard <oyebenard@gmail.com>
2024-08-23 16:00:55 -0500

    Create new arrival section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import HeroBanner from "./components/HeroBanner";
+import NewArrival from "./components/NewArrival";
 
 function App() {
   return (
     <div className="h-full">
       <Header />
       <HeroBanner />
+      <NewArrival />
     </div>
   );
 }
