948061cfd320dac8867f9a8fe12b5b61f885cc0e
Bernard <oyebenard@gmail.com>
2024-08-30 20:21:29 -0500

    Update detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 import { TfiHeart } from "react-icons/tfi";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { PiCaretDown } from "react-icons/pi";
+import { v4 as uuidv4 } from 'uuid';
 
 function Detail({ product, currentArticle, changeArticle }) {
-    // console.log(product.articlesList);
     const [expandedSection, setExpandedSection] = useState(null);
     const [thumbnails, setThumbnails] = useState([]);
     const [selectedThumbnailCode, setSelectedThumbnailCode] = useState(null);
@@ -33,12 +33,15 @@ function Detail({ product, currentArticle, changeArticle }) {
             setSelectedThumbnailCode(tempThumbnails[0].code);
         }
 
-        setThumbnails(tempThumbnails);
+        tempThumbnails.length > 12 ? setThumbnails(tempThumbnails.splice(0, 12)) : setThumbnails(tempThumbnails);
+        console.log('remounted');
     }, []);
 
     const handleArticleChange = (code) => {
         changeArticle(code);
         setSelectedThumbnailCode(code);
+        console.log(currentArticle);
+        console.log(product);
     }
 
     const handleSizeChange = (name) => {
@@ -76,6 +79,7 @@ function Detail({ product, currentArticle, changeArticle }) {
                 </h3>
                 <div className="grid grid-cols-4 gap-1 text-center text-lg">
                     {currentArticle.variantsList.map(variant => (
+                        Object.keys(variant).length > 0 &&
                         <button
                             onClick={() => handleSizeChange(variant.size.name)}
                             key={variant.code}
@@ -105,48 +109,64 @@ function Detail({ product, currentArticle, changeArticle }) {
             <div className="mt-6">
                 <div className="border-b">
                     <button
-                        className={`w-full text-left py-4 flex justify-between items-center focus:outline-none ${expandedSection === 'description' && 'text-red-600'}`}
+                        className={`w-full text-left py-4 flex justify-between items-center hover:text-red-600 hover:transition-all ease-in-out duration-300 focus:outline-none ${expandedSection === 'description' && 'text-red-600'}`}
                         onClick={() => toggleExpand('description')}
                     >
                         <span className="font-semibold">Description & fit</span>
                         <span
-                            className={`transform transition-transform duration-200 ${expandedSection === 'description' ? 'rotate-180' : ''
-                                }`}
+                            className={`transform transition-transform duration-200 ${expandedSection === 'description' ? 'rotate-180' : ''}`}
                         >
                             <PiCaretDown />
                         </span>
                     </button>
-                    {expandedSection === 'description' && (
-                        <div>
-                            {
+                    <div
+                        className={`overflow-hidden transition-max-height duration-500 ease-in-out ${expandedSection === 'description' ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+                    >
+                        <div className="transition-opacity duration-500 ease-in-out">
+                            {expandedSection === 'description' && (
                                 <div>
-                                    {product.hasOwnProperty('sellingAttributes') && product.sellingAttributes.includes('New Arrival') &&
-                                        <p className="text-xs pb-2">New Arrival</p>}
-                                    <p className="pb-4 text-[15px]">{product.description}</p>
-                                    {/* <p>
-                                        <span className="text-sm font-semibold">Size: </span>
-                                        <span></span>
-                                    </p> */}
-                                    <p className="text-sm">
-                                        <span className="font-semibold">Length: </span>
-                                        <span>{product.lengthCollection.find(x => x.code === 'garmentLength').value[0]}</span>
-                                    </p>
-                                    <p className="text-sm">
-                                        <span className="font-semibold">Length: </span>
-                                        <span>{product.lengthCollection.find(x => x.code === 'garmentLength').value[0]}</span>
-                                    </p>
+                                    <div>
+                                        {currentArticle.sellingAttributes?.includes('New Arrival') && (
+                                            <p className="text-xs pb-2">New Arrival</p>
+                                        )}
+
+                                        <p className="pb-4 text-[15px]">{currentArticle.description}</p>
+
+                                        <div className="pb-6">
+                                            {product.lengthCollection.length > 0 && (
+                                                <p className="text-[13px]">
+                                                    <span className="font-semibold">Length: </span>
+                                                    <span>{product.lengthCollection.find(x => x.code === 'garmentLength')?.value[0]}</span>
+                                                </p>
+                                            )}
+
+                                            {product.fits.length > 0 && (
+                                                <p className="text-[13px]">
+                                                    <span className="font-semibold">Fit: </span>
+                                                    <span>{product.fits[0]}</span>
+                                                </p>
+                                            )}
+
+                                            <p className="text-[13px]">
+                                                <span className="font-semibold">Description: </span>
+                                                <span>
+                                                    {`${currentArticle.colourDescription}, ${currentArticle.pattern ? currentArticle.pattern : currentArticle.textualPrint}`}
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
                                 </div>
-                            }
+                            )}
                         </div>
-                    )}
+                    </div>
                 </div>
 
                 <div className="border-b">
                     <button
-                        className={`w-full text-left py-4 flex justify-between items-center focus:outline-none ${expandedSection === 'materials' && 'text-red-600'}`}
+                        className={`w-full text-left py-4 flex justify-between items-center hover:text-red-600 hover:transition-all ease-in-out duration-300 focus:outline-none ${expandedSection === 'materials' && 'text-red-600'}`}
                         onClick={() => toggleExpand('materials')}
                     >
-                        <span className="font-semibold">Materials</span>
+                        <span className="font-semibold hover:text-red-600">Materials</span>
                         <span
                             className={`transform transition-transform duration-200 ${expandedSection === 'materials' ? 'rotate-180' : ''
                                 }`}
@@ -154,14 +174,120 @@ function Detail({ product, currentArticle, changeArticle }) {
                             <PiCaretDown />
                         </span>
                     </button>
-                    {expandedSection === 'materials' && (
-                        <div className="p-4">
-                            <p className="text-gray-600">Material details and composition...</p>
+                    <div
+                        className={`overflow-hidden transition-max-height duration-500 ease-in-out ${expandedSection === 'materials' ? 'max-h-[40rem] opacity-100' : 'max-h-0 opacity-0'}`}
+                    >
+                        {expandedSection === 'materials' && (
+                            <div className="pb-6">
+                                <p className="font-semibold text-[15px]">Composition</p>
+                                {/* composition is of one type */}
+                                {
+                                    currentArticle.compositions && currentArticle.compositions.length === 1 &&
+                                    currentArticle.compositions.map(composition => (
+                                        <p className="text-[13px] pt-1">
+                                            <p>
+                                                {composition.materials.map((material, index) => (
+                                                    <span key={uuidv4()}>{`${material.name} `}{`${Number.parseInt(material.percentage).toFixed(0)}%`}{index !== composition.materials.length - 1 && ', '}</span>
+                                                ))}
+                                            </p>
+                                        </p>
+                                    ))
+                                }
+                                {/* composition is of more than one type */}
+                                {
+                                    currentArticle.compositions && currentArticle.compositions.length > 1 &&
+                                    currentArticle.compositions.map(composition => (
+                                        <p className="text-[13px] pt-1">
+                                            <span className="font-semibold">{`${composition.compositionType} : `}</span>
+                                            <span>{composition.materials.map((material, index) => (
+                                                <span key={uuidv4()}>
+                                                    <span>{`${material.name} `}{`${Number.parseInt(material.percentage).toFixed(0)}%`}{index !== composition.materials.length - 1 ? ', ' : ''}</span>
+                                                </span>
+                                            ))}</span>
+                                        </p>
+                                    ))
+                                }
+                                <p className="font-semibold pt-6 text-[15px]">Additional material information</p>
+                                <div className="text-[13px] pt-1">
+                                    <p>The total weight of this product contains:</p>
+                                    {currentArticle.sustainabilityCompositions && currentArticle.sustainabilityCompositions.length > 0 &&
+                                        <ul key={uuidv4()}>
+                                            {currentArticle.sustainabilityCompositions[0].materials.map(material => (
+                                                <li className="list-disc list-inside">{Number.parseInt(material.percentage).toFixed(0)}% {material.name}</li>
+                                            ))}
+                                        </ul>
+                                    }
+                                </div>
+                                <p className="font-semibold text-[15px] pt-6">Materials in this product explained</p>
+                                {currentArticle.materialDetails && currentArticle.materialDetails.length > 1 &&
+                                    currentArticle.materialDetails.map(detail => (
+                                        <div key={uuidv4()} className="pt-3">
+                                            <p className="font-semibold text-[13px]">{detail.name}</p>
+                                            <p className="text-[13px] pt-2">{detail.description}</p>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+                {currentArticle.careInstructions && currentArticle.careInstructions.length > 0 &&
+                    <div className="border-b">
+                        <button
+                            className={`w-full text-left py-4 flex justify-between items-center hover:text-red-600 hover:transition-all ease-in-out duration-300 focus:outline-none ${expandedSection === 'careGuide' && 'text-red-600'}`}
+                            onClick={() => toggleExpand('careGuide')}
+                        >
+                            <span className="font-semibold hover:text-red-600">Care Guide</span>
+                            <span
+                                className={`transform transition-transform duration-200 ${expandedSection === 'careGuide' ? 'rotate-180' : ''
+                                    }`}
+                            >
+                                <PiCaretDown />
+                            </span>
+                        </button>
+                        <div
+                            className={`overflow-hidden transition-max-height duration-500 ease-in-out ${expandedSection === 'careGuide' ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+                        >
+                            {expandedSection === 'careGuide' && (
+                                currentArticle.careInstructions && currentArticle.careInstructions.length > 0 &&
+                                <ul className="text-[13px] pb-6">
+                                    {currentArticle.careInstructions.map(instruction => (
+                                        <li key={uuidv4()} className="list-disc list-inside">{instruction}</li>
+                                    ))}
+                                </ul>
+                            )}
                         </div>
-                    )}
+                    </div>
+                }
+            </div>
+
+            <div className="border-b">
+                <button
+                    className={`w-full text-left py-4 flex justify-between items-center hover:text-red-600 hover:transition-all ease-in-out duration-300 focus:outline-none ${expandedSection === 'deliveryAndPayment' && 'text-red-600'}`}
+                    onClick={() => toggleExpand('deliveryAndPayment')}
+                >
+                    <span className="font-semibold hover:text-red-600">Delivery Payment</span>
+                    <span
+                        className={`transform transition-transform duration-200 ${expandedSection === 'deliveryAndPayment' ? 'rotate-180' : ''
+                            }`}
+                    >
+                        <PiCaretDown />
+                    </span>
+                </button>
+                <div
+                    className={`text-[13px] overflow-hidden transition-max-height duration-500 ease-in-out ${expandedSection === 'deliveryAndPayment' ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0'}`}
+                >
+                    <p className="pb-2">Members get free online returns</p>
+                    <p>
+                        Delivery: Shipping is available to an address within Canada.
+                        Payment: We accept credit card payments via MasterCard and Visa.
+                        You can also pay with PayPal or Tolus giftcards. Learn more on our
+                        customer service page
+                    </p>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
