0382d39efe573a165b26dd9e722ec62f5a6bf504
Bernard <oyebenard@gmail.com>
2024-09-24 21:59:48 -0500

    Optimize new arrivals update

--- src/App.js
+++ src/App.js
@@ -16,88 +16,109 @@ function App() {
   const [newArrivals, setNewArrivals] = useState([]);
   const [categories, setCategories] = useState([]);
 
-  const optionsBaseApparel = {
-    method: 'GET',
-    url: process.env.REACT_APP_API_URL,
-    params: {
-      country: 'ca',
-      lang: 'en',
-      currentpage: '0',
-      pagesize: '30',
-    },
-    headers: {
-      'x-rapidapi-key': process.env.REACT_APP_API_KEY,
-      'x-rapidapi-host': process.env.REACT_APP_API_HOST
+  const fetchBaseApparel = async (currentPage = 0) => {
+    const optionsBaseApparel = {
+      method: 'GET',
+      url: process.env.REACT_APP_API_URL,
+      params: {
+        country: 'ca',
+        lang: 'en',
+        currentpage: currentPage,
+        pagesize: '30',
+      },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_API_KEY,
+        'x-rapidapi-host': process.env.REACT_APP_API_HOST
+      }
+    };
+
+    try {
+      const response = await axios.request(optionsBaseApparel);
+      const baseApparel = [...response.data.results];
+      setBaseApparel(prev => [...prev, ...baseApparel]);
+      return baseApparel;
+    }
+    catch (error) {
+      console.error(error);
+      return [];
     }
   };
 
-  const optionsCategory = {
-    method: 'GET',
-    url: 'https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/categories/list',
-    params: {
-      lang: 'en',
-      country: 'us'
-    },
-    headers: {
-      'x-rapidapi-key': '539f84e7fcmsh4984cab77c02428p1da61ejsnc1e79160e58c',
-      'x-rapidapi-host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+  const fetchCategories = async () => {
+    const optionsCategory = {
+      method: 'GET',
+      url: 'https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/categories/list',
+      params: {
+        lang: 'en',
+        country: 'us'
+      },
+      headers: {
+        'x-rapidapi-key': '539f84e7fcmsh4984cab77c02428p1da61ejsnc1e79160e58c',
+        'x-rapidapi-host': 'apidojo-hm-hennes-mauritz-v1.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(optionsCategory);
+      const categories = [...response.data];
+      const filteredCategories = categories.filter(cat => cat.tagCodes.length > 0).filter(cat => cat.CatName !== 'Divided' && cat.CatName !== 'Beauty' && cat.CatName !== 'Sport')
+      setCategories(filteredCategories);
     }
+    catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateNewArrivals = async (baseApparel, currentPage) => {
+    const validNewArrivals = [];
+    let index = 0;
+
+    while (validNewArrivals.length < 10 && index < baseApparel.length) {
+      const item = baseApparel[index];
+      try {
+        await new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = item.images[0].baseUrl;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+
+        if (item.hasOwnProperty('sellingAttributes') && item.sellingAttributes.includes('New Arrival')) {
+          validNewArrivals.push(item);
+        }
+
+      } catch {
+        console.warn(`Image failed to load for item: ${item.code}, trying next item...`);
+      }
+      index++;
+    }
+
+    // If we don't have enough new arrivals, fetch more from the next page
+    if (validNewArrivals.length < 10 && currentPage < 5) {
+      console.log(validNewArrivals.length);
+
+      const nextPage = currentPage + 1;
+      const moreBaseApparel = await fetchBaseApparel(nextPage);
+      const moreNewArrivals = await updateNewArrivals(moreBaseApparel, nextPage);
+      const remainingCount = 10 - validNewArrivals.length;
+      const remainderNewArrivals = moreNewArrivals.slice(0, remainingCount);
+
+      return [...validNewArrivals, ...remainderNewArrivals];
+    }
+
+
+    return validNewArrivals;
   };
 
   useEffect(() => {
-    const fetchBaseApparel = async () => {
-      try {
-        const response = await axios.request(optionsBaseApparel);
-        const baseApparel = [...response.data.results];
-        setBaseApparel(baseApparel);
-        updateNewArrivals(baseApparel);
-      }
-      catch (error) {
-        console.error(error);
-      }
+    const fetchInitialData = async () => {
+      const baseApparel = await fetchBaseApparel();
+      const newArrivals = await updateNewArrivals(baseApparel, 0);
+      setNewArrivals(newArrivals);
+      fetchCategories();
     };
 
-    const fetchCategories = async () => {
-      try {
-        const response = await axios.request(optionsCategory);
-        const categories = [...response.data];
-        const filteredCategories = categories.filter(cat => cat.tagCodes.length > 0).filter(cat => cat.CatName !== 'Divided' && cat.CatName !== 'Beauty' && cat.CatName !== 'Sport')
-        setCategories(filteredCategories);
-      }
-      catch (error) {
-        console.error(error);
-      }
-    };
-
-    const updateNewArrivals = async (baseApparel) => {
-      const validNewArrivals = [];
-      let index = 0;
-
-      while (validNewArrivals.length < 10 && index < baseApparel.length) {
-        const item = baseApparel[index];
-        try {
-          await new Promise((resolve, reject) => {
-            const img = new Image();
-            img.src = item.images[0].baseUrl;
-            img.onload = resolve;
-            img.onerror = reject;
-          });
-
-          if (item.hasOwnProperty('sellingAttributes') && item.sellingAttributes.includes('New Arrival')) {
-            validNewArrivals.push(item);
-          }
-
-        } catch {
-          console.warn(`Image failed to load for item: ${item.code}, trying next item...`);
-        }
-        index++;
-      }
-
-      setNewArrivals(validNewArrivals);
-    }
-
-    fetchBaseApparel();
-    fetchCategories();
+    fetchInitialData();
   }, []);
 
   return (
